6b9a5aaff11d891458973e40a22e6b212c2f45a1
Gabriel Volpe <volpegabriel@gmail.com>
2017-09-20 17:07:48 +0300

    Parsing Wikipedia visa requirements data.

--- src/main/scala/com/github/gvolpe/smartbackpacker/SmartBackpacker.scala
+++ src/main/scala/com/github/gvolpe/smartbackpacker/SmartBackpacker.scala
@@ -16,15 +16,33 @@ object SmartBackpacker extends App {
 
   val httpClient = PooledHttp1Client[IO]()
 
-
 //  case class Revision(contentformat: String, contentmodel: String)
 //
 //  case class VisaRequirementsRawJson(query: )
 
+  val travelingTo = "China"
+
   val result: IO[Json] = httpClient.expect[Json](wikiUrl + pageId)
   val value = result.unsafeRunSync().noSpaces
 
-  println(value.split("excluding departure fees")(1).drop(9))
+  val content = value.split("excluding departure fees")(1).drop(9)
+
+  val index = content.indexOf(travelingTo)
+  val parsed = content.substring(index + travelingTo.length + 8, index + travelingTo.length + 1200).split("}}")
+
+  parsed.foreach(println)
+
+  val startingIndex = if (parsed(0).contains("Visa")) 0 else 1
+
+  val visaRequirement = parsed(startingIndex).split('|').toList.last
+  val visaDays = parsed(startingIndex + 2).drop(10).split("<ref>")(0).split('\\')(0)
+    .replaceAll("\\[", "")
+    .replaceAll("\\]", "")
+    .replaceAll("<br>", " ")
+    .split('{')(0)
+    .trim
+
+  println(s"$visaRequirement => $visaDays")
 
   httpClient.shutdownNow()
 
