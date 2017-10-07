cf84334351809a180a243c2daeeebd1b84298187
Gabriel Volpe <volpegabriel@gmail.com>
2017-10-07 18:25:02 +0300

    Binding host...

--- src/main/scala/com/github/gvolpe/smartbackpacker/Server.scala
+++ src/main/scala/com/github/gvolpe/smartbackpacker/Server.scala
@@ -10,7 +10,7 @@ object Server extends StreamApp[IO] {
 
   override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, Nothing] =
     BlazeBuilder[IO]
-      .bindHttp(sys.env.getOrElse("PORT", "8080").toInt, "localhost")
+      .bindHttp(sys.env.getOrElse("PORT", "8080").toInt, "0.0.0.0")
       .mountService(DestinationInfoHttpEndpoint.service)
       .mountService(AirlineReviewHttpEndpoint.service)
       .serve
