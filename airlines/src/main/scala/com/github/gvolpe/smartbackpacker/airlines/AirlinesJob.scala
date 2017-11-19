f413eb63feeff7522fcede277288f2d25413195f
Gabriel Volpe <volpegabriel@gmail.com>
2017-11-19 18:02:02 +0100

    Adding license file and changing env variables for main args variables.

--- airlines/src/main/scala/com/github/gvolpe/smartbackpacker/airlines/AirlinesJob.scala
+++ airlines/src/main/scala/com/github/gvolpe/smartbackpacker/airlines/AirlinesJob.scala
@@ -7,28 +7,27 @@ import com.github.gvolpe.smartbackpacker.airlines.sql.{AirlinesCreateTables, Air
 // See: https://wikitravel.org/en/Discount_airlines_in_Europe
 object AirlinesJob extends IOApp {
 
-  case object AirlineFileNotFound extends Exception("Environment variable not defined: SB_AIRLINES_FILE.")
-  case object AllowanceFileNotFound extends Exception("Environment variable not defined: SB_AIRLINES_FILE.")
+  case object AirlineFileNotFound extends Exception("Airlines file not defined")
+  case object AllowanceFileNotFound extends Exception("Allowance file not defined")
 
-  val readSystemEnv: IO[(AirlineFile, AllowanceFile)] = {
+  def readArgs(args: List[String]): IO[(AirlineFile, AllowanceFile)] = {
     for {
-      _ <- IO { println("Reading environment variables") }
-      x <- sys.env.get("SB_AIRLINES_FILE").fold(IO.raiseError[String](AirlineFileNotFound))(a => IO(a))
-      y <- sys.env.get("SB_ALLOWANCE_FILE").fold(IO.raiseError[String](AllowanceFileNotFound))(a => IO(a))
+      x <- args.headOption.fold(IO.raiseError[String](AirlineFileNotFound))(a => IO(a))
+      y <- args.lastOption.fold(IO.raiseError[String](AllowanceFileNotFound))(a => IO(a))
     } yield (new AirlineFile(x), new AllowanceFile(y))
   }
 
   def program(airlineFile: AirlineFile, allowanceFile: AllowanceFile): IO[Unit] =
     for {
       _       <- IO { println("Starting job") }
-//      _       <- AirlinesCreateTables[IO].run
+      _       <- AirlinesCreateTables[IO].run
       _       <- AirlinesInsertData[IO](airlineFile, allowanceFile).run
       _       <- IO { println("Job finished successfully") }
     } yield ()
 
   override def start(args: List[String]): IO[Unit] =
     for {
-      files   <- readSystemEnv
+      files   <- readArgs(args)
       (x, y)  = files
       _       <- program(x, y)
     } yield ()
