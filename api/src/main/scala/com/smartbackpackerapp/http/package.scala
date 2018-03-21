c414443ce00f890892f9951c31296f3d45c2005f
Gabriel Volpe <volpegabriel@gmail.com>
2018-03-21 21:28:22 +0900

    Upgrading fs2, cats effect, monix + some refactors.

--- api/src/main/scala/com/smartbackpackerapp/http/package.scala
+++ api/src/main/scala/com/smartbackpackerapp/http/package.scala
@@ -17,7 +17,9 @@
 package com.smartbackpackerapp
 
 import com.smartbackpackerapp.model._
-import io.circe.{Encoder, Json}
+import io.circe.Encoder
+import io.circe.generic.extras.decoding.UnwrappedDecoder
+import io.circe.generic.extras.encoding.UnwrappedEncoder
 
 package object http {
 
@@ -32,61 +34,9 @@ package object http {
 
   case class ApiError(code: ApiErrorCode, error: String)
 
-  // --- Json encoders ---
-  implicit val apiErrorCodeEncoder: Encoder[ApiErrorCode] = Encoder.instance {
-    x => Json.fromString(x.value.toString)
-  }
+  implicit def valueClassEncoder[A: UnwrappedEncoder]: Encoder[A] = implicitly
 
-  implicit val countryCodeEncoder: Encoder[CountryCode] = Encoder.instance {
-    x => Json.fromString(x.value)
-  }
-
-  implicit val countryNameEncoder: Encoder[CountryName] = Encoder.instance {
-    x => Json.fromString(x.value)
-  }
-
-  implicit val currencyEncoder: Encoder[Currency] = Encoder.instance {
-    x => Json.fromString(x.value)
-  }
-
-  implicit val airlineNameEncoder: Encoder[AirlineName] = Encoder.instance {
-    x => Json.fromString(x.value)
-  }
-
-  implicit val visaCategoryEncoder: Encoder[VisaCategory] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
-
-  implicit val baggageTypeEncoder: Encoder[BaggageType] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
-
-  implicit val visaRankingEncoder: Encoder[Ranking] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
-
-  implicit val rankingCountTypeEncoder: Encoder[Count] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
-
-  implicit val rankingSharingEncoder: Encoder[Sharing] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
-
-  implicit val alertLevelEncoder: Encoder[AlertLevel] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
-
-  implicit val diseaseEncoder: Encoder[Disease] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
-
-  implicit val diseaseCategoryEncoder: Encoder[DiseaseCategory] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
-
-  implicit val webLinkEncoder: Encoder[WebLink] = Encoder.instance {
-    x => Json.fromString(x.toString)
-  }
+  //implicit def jsonDecoder[A <: Product : Decoder](implicit F: Sync[F]): EntityDecoder[F, A] = jsonOf[F, A]
+  //implicit def jsonEncoder[A <: Product : Encoder](implicit F: Sync[F]): EntityEncoder[F, A] = jsonEncoderOf[F, A]
 
 }
